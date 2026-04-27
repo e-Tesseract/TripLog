60361bafcbf85f9de32cbc54e42e68e6f6c984c8
Capon Hugo <capon.hugo@gmail.com>
2026-04-27 11:10:41 +0200

    Ajout swagger et modifs appels api

--- triplog_back/app.js
+++ triplog_back/app.js
@@ -10,6 +10,10 @@ const stepsRoutes = require('./routes/steps');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const citiesRoutes = require('./routes/cities.js');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+
   
 const app = express();
 const PORT = process.env.PORT || 3000;
