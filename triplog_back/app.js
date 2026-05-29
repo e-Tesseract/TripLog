92b0b334aac8d05df3d4c2e134d8aacfc10d1c31
Capon Hugo <capon.hugo@gmail.com>
2026-05-29 14:34:10 +0200

    Update app.js

--- triplog_back/app.js
+++ triplog_back/app.js
@@ -9,7 +9,6 @@ const stepsRoutes = require('./routes/steps');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
-const citiesRoutes = require('./routes/cities.js');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
